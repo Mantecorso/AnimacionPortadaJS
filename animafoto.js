b954901c11d138c6041c34c9fa31288c37b58d08
Mantecorso <luisjuradoquesada@gmail.com>
2021-11-14 12:54:36 +0100

    archivo JS

---
+++ animafoto.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+
+    const banner = this.document.getElementsByClassName('banner')[0];
+    const blocks = this.document.getElementsByClassName('blocks');
+
+    for (var i = 0; i < 400; i++) {
+        banner.innerHTML += "<div class = 'blocks'></div>";
+        blocks[i].style.animationDelay = `${i * 0.05}s`;
+    }
+})
